7798f8dc29f9e7eb84c1602a0230a10089bbc517
Joel Maciel <soldado25461@gmail.com>
2022-04-28 18:12:58 -0300

    Criando pagina filme parte 2

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import api from "../../service/api";
 import "./filme-info.css";
 
@@ -7,15 +7,25 @@ export default function Filme() {
   const { id } = useParams();
   const [filme, setFilme] = useState([]);
   const [loading, setLoading] = useState(true);
+  const history = useHistory();
 
   useEffect(() => {
     async function loadFilme() {
       const response = await api.get(`r-api/?api=filmes/${id}`);
+
+      if (response.data.length === 0) {
+        history.replace("/");
+        return;
+      }
       setFilme(response.data);
       setLoading(false);
     }
     loadFilme();
-  }, [id]);
+
+    return () => {
+      console.log("COMPONENTE DESMONTADO.");
+    };
+  }, [history, id]);
 
   if (loading) {
     return (
@@ -26,7 +36,18 @@ export default function Filme() {
   }
   return (
     <div className="filme-info">
-      <h1>Filme - {id}</h1>
+      <h1>{filme.nome}</h1>
+      <img src={filme.foto} alt={filme.nome} />
+      <h3>Sinopse</h3>
+      {filme.sinopse}
+      <div className="botoes">
+        <button onClick={() => {}}>Salvar</button>
+        <button>
+          <a target="_blank" href={`https://youtube.com/results?search_query=${filme.nome} Trailer`} rel="noreferrer">
+            Trailer
+          </a>
+        </button>
+      </div>
     </div>
   );
 }
